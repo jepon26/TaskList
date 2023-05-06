ba1eb82c5abf1966bd6c9d64f71787ea6820e83a
Elvin Javier Rueda <elvinrueda@yahoo.es>
2023-05-05 23:00:19 -0600

    initial commit

---
+++ todo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './img//todologo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className='todo-app'>
+     <div className='todo-logo-container'>
+        <img src={logo} className="todo-logo" alt="logo" />
+        </div>
+         
+    </div>
+  );
+}
+
+export default App;
